285e1c4c02e7c75b98ee7134e4bc984c4ef9f616
Todd Kinsman <todd.kinsman@gmail.com>
2016-10-26 14:14:57 -0500

    added correct readme

---
+++ src/main/java/edu/matc/service/StashitApplication.java
@@ -0,0 +1,7 @@
+package edu.matc.service;
+
+/**
+ * Created by toddkinsman on 10/26/16.
+ */
+public class StashitApplication {
+}
---
+++ src/main/java/edu/matc/service/UserService.java
@@ -0,0 +1,7 @@
+package edu.matc;
+
+/**
+ * Created by toddkinsman on 10/26/16.
+ */
+public class UserService {
+}
